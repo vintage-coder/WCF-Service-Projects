a7ccfcc607f50be5ae7f5e075858d071ecf34e41
FAREAST\v-gowthamanv <gowthamanvai@gmail.com>
2022-06-18 15:37:47 +0530

    hello app wcf service initial commit

---
+++ HelloApps/HelloService/HelloServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host=new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+
+                Console.WriteLine("Host started @" + DateTime.Now.ToString());
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
